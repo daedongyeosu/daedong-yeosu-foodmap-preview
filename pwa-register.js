a2e12b4dff747c175134632f863038023b6316da
daedongyeosu <sisakim@naver.com>
2026-08-02 20:29:25 +0900

    서비스워커와 메뉴맵 최신파일 강제 확인

--- pwa-register.js
+++ pwa-register.js
@@ -2,7 +2,11 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', {scope: '/'})
+    navigator.serviceWorker.register('/sw.js', {
+      scope: '/',
+      updateViaCache: 'none'
+    })
+      .then(registration => registration.update())
       .catch(() => {
         // 앱 설치 지원 실패가 음식지도 이용을 막지 않도록 조용히 무시합니다.
       });
