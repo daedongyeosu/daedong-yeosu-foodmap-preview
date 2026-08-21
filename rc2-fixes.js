1d4f959ee5acc463529e35a41d3ba28530fcf1a1
daedongyeosu <sisakim@naver.com>
2026-08-21 19:02:27 +0900

    chore: rc2-fixes.js 줄바꿈 보존

--- rc2-fixes.js
+++ rc2-fixes.js
@@ -1139,4 +1139,3 @@ fxInitialize = async function rc2Initialize() {
   setTimeout(() => { rc2DeferredStoreReturnPosition = null; }, 2200);
   rc2StartAmbient(!restoredStore && !restoredAppBrowser);
 };
-
